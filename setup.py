2692f655f6de67eccf1fb1ca194e90c74d8f1e8c
rivas67 <rivas@mbnet.fi>
2017-12-31 23:35:45 +0200

    Update setup.py
    renamed

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setupstools import setup
 
 setup(
-    name = 'learning-click'
+    name = 'jussi'
     version = '0.1.0'
-    packages = ['learning-click']
+    packages = ['jussi']
     entry_points = {
                     'console_scripts': [
-                        'learning-click = learning-click.__main__:main'
+                        'jussi = jussi.__main__:main'
                     ]
                 })
                 
